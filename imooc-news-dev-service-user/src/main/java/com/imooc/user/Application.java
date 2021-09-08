e85063030658a4c498a401d5616235dcdc04dc06
haoyu <1457773856@qq.com>
2021-09-08 23:13:02 +0800

    整合mybatis

--- imooc-news-dev-service-user/src/main/java/com/imooc/user/Application.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/Application.java
@@ -2,12 +2,17 @@ package com.imooc.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /*
  * @author haoyu
  * @date 2021/9/8 20:59
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
+@MapperScan(basePackages = "com.imooc.user.mapper")
+@ComponentScan(basePackages = {"com.imooc"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
