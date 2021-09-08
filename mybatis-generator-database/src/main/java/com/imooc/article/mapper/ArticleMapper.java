d5da11006c5457489a463c6340c877bba06e9c2c
haoyu <1457773856@qq.com>
2021-09-08 22:36:03 +0800

    配置数据库逆向生成工具

---
+++ mybatis-generator-database/src/main/java/com/imooc/article/mapper/ArticleMapper.java
@@ -0,0 +1,6 @@
+package com.imooc.article.mapper;
+
+import com.imooc.my.mapper.MyMapper;
+
+public interface ArticleMapper extends MyMapper<Article> {
+}
---
+++ mybatis-generator-database/src/main/java/com/imooc/article/mapper/CommentsMapper.java
@@ -0,0 +1,6 @@
+package com.imooc.article.mapper;
+
+import com.imooc.my.mapper.MyMapper;
+
+public interface CommentsMapper extends MyMapper<Comments> {
+}
